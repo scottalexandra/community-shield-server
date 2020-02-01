f07179e304831291c1ea5aa0ffddbcaf3686f7ac
Alex Robinson <alexandra.scott335@gmail.com>
2020-01-31 22:30:09 -0700

    set session and csrf cookies, add middleware to use cookies and authenticity tokens for api

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -2,9 +2,8 @@ class Api::V1::SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
-      payload = { user_id: user.id }
-      jwt = JWT.encode payload, ENV['secret'], 'HS256'
-      render json: UserSerializer.new(user), meta: jwt, meta_key: 'jwt'
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user)
     else
       error = 'Email or password invalid. Please try again.'
       render json: ErrorSerializer.new(error), status: :unauthorized
